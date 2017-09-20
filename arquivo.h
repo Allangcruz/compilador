c127cc89fb2f2779cab7f72ea3a012d62805947c
Allan Gonçalves da Cruz <allangcruz@gmail.com>
2017-09-20 16:31:27 -0300

    Criando projeto com arquivos separados

---
+++ arquivo.h
@@ -0,0 +1,16 @@
+/*
+Alunos:
+Allan Gonçalves da Cruz
+Elson Bento dos Santos
+
+Responsavel por manipular a leitura de arquivos.
+
+*/
+
+/**
+ * Ler arquivo conteudo de arquivo
+ */
+void lerArquivo() {
+	printf("Modulo de arquivo carregado ...\n");
+}
+
---
+++ compilador.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#include "leituraArquivo.h"
+
+/*
+Alunos:
+Allan Gonçalves da Cruz
+Elson Bento dos Santos
+
+Fases do compilador
+1 - Definir estrutura
+2 - Ler Arquivo
+3 - Definir lista de simbolos
+4 - Definir palavras reservadas (utilizar lista encadeada)
+
+*/
+
+void main (int argc, char** argv) {
+    printf("Hello World!\n");
+    
+    lerArquivo();
+}
+
